3bf9b0504853d0d54d3439817580bfa80c1b6ffd
Mehdi Druon <mehdidruon@gmail.com>
2017-04-26 12:35:15 +0200

    ajout fonctionnalités

--- public/client.js
+++ public/client.js
@@ -1,5 +1,7 @@
 /*jslint browser: true*/
-const socket = io();
+var socket = io();
+var typingTimer;
+var isTyping = false;
 
 function scrollToBottom() {
   if ($(window).scrollTop() + $(window).height() + 2 * $('#messages li').last().outerHeight() >= $(document).height()) {
@@ -15,41 +17,69 @@ $('form').submit(function(e) {
   socket.emit('chat-message', message);
   $('#m').val('');
   $('chat-input').focus();
-
 });
+
+$('#login form').submit(function (e) {
+  e.preventDefault();
+  var user = {
+    username : $('#login input').val().trim()
+  };
+  if (user.username.length > 0) { // Si le champ de connexion n'est pas vide
+  socket.emit('user-login', user, function (success) {
+    if (success) {
+      $('body').removeAttr('id'); // Cache formulaire de connexion
+      $('#chat input').focus(); // Focus sur le champ du message
+    }
+  });
+}
+});
+
+// Affiche le message sur le chat
 socket.on('chat-message', function(message){
   $('#messages').append($('<li>').html('<span class="username">' + message.username + '</span>' + message.text));
   scrollToBottom();
 });
-
+// Affiche le message de service sur le chat
 socket.on('service-message', function (message) {
   $('#messages').append($('<li class="' + message.type + '">').html('<span class="info">information</span> ' + message.text));
   scrollToBottom();
 });
-  // Connexion d'un utilisateur
-  socket.on('user-login', function (user) {
-    $('#users').append($('<li class="' + user.username + ' new">').html(user.username));
-      setTimeout(function () {
-        $('#users li.new').removeClass('new');
-      }, 1000);
-    });
-    // Deconnexion d'un utilisateur
-    socket.on('user-logout', function (user) {
-      var selector = '#users li.' + user.username;
-      $(selector).remove();
-    });
 
-    $('#login form').submit(function (e) {
-    e.preventDefault();
-    var user = {
-      username : $('#login input').val().trim()
-    };
-    if (user.username.length > 0) { // Si le champ de connexion n'est pas vide
-      socket.emit('user-login', user, function (success) {
-        if (success) {
-          $('body').removeAttr('id'); // Cache formulaire de connexion
-          $('#chat input').focus(); // Focus sur le champ du message
-        }
-      });
+// Savoir si un utilisateur est en train d'écrire
+$('#m').keypress(function () {
+  clearTimeout(typingTimer);
+  if (!isTyping) {
+    socket.emit('start-typing');
+    isTyping = true;
+  }
+});
+// Mettre à jour s'il arrête d'écrire
+$('#m').keyup(function () {
+  clearTimeout(typingTimer);
+  typingTimer = setTimeout(function () {
+    if (isTyping) {
+      socket.emit('stop-typing');
+      isTyping = false;
     }
-  });
+  }, 200);
+});
+// mettre à jour l'UI, gestion de saisie des autres utilisateurs
+socket.on('update-typing', function (typingUsers) {
+  $('#users li span.typing').hide();
+  for (i = 0; i < typingUsers.length; i++) {
+    $('#users li.' + typingUsers[i].username + ' span.typing').show();
+  }
+});
+
+// Connexion d'un utilisateur
+socket.on('user-login', function (user) {
+  $('#users').append($('<li class="' + user.username + ' new">').html(user.username + '<span class="typing">typing</span>'));
+  setTimeout(function () {
+    $('#users li.new').removeClass('new');
+  }, 1000);
+});
+// Deconnexion d'un utilisateur
+socket.on('user-logout', function (user) {
+  var selector = '#users li.' + user.username;
+  $(selector).remove();
+});
--- server.js
+++ server.js
@@ -4,22 +4,38 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var loggedUser;
 var users = [];
+var messages = [];
+var typingUsers = [];
 
 app.use("/", express.static(__dirname + "/public"));
 
 io.on('connection', function(socket) {
-  console.log('a user connected'); //pour savoir quand un utilisateur arrive
-
+  //pour savoir quand un utilisateur arrive
+  console.log('a user connected');
+  // boucle pour ajouter les users dans le tableau
   for (i = 0; i < users.length; i++) {
     socket.emit('user-login', users[i]);
   };
+  // boucle pour ajouter les messages dans le tableau pour que la conversation ne soit pas bloquée
+  for (i = 0; i < messages.length; i++) {
+    if (messages[i].username !== undefined) {
+      socket.emit('chat-message', messages[i]);
+    } else {
+      socket.emit('service-message', messages[i]);
+    }
+  }
 
-  socket.on('user-login', function(loggedUser) { //login
+// Pour logger le nouvel utilisateur
+  socket.on('user-login', function(loggedUser) {
     console.log('user logged in : ' + loggedUser.username);
     user = loggedUser;
-    socket.on('chat-message', function(message) { //chat avec nom d'utilisateur
+    socket.on('chat-message', function (message) {
       message.username = loggedUser.username;
       io.emit('chat-message', message);
+      messages.push(message);
+      if (messages.length > 150) {
+        messages.splice(0, 1);
+      }
     });
   });
   socket.on('user-login', function(user, callback) {
@@ -30,8 +46,7 @@ io.on('connection', function(socket) {
         userIndex = i;
       }
     }
-      if (user !== undefined && userIndex === -1) { // S'il est bien nouveau
-        // Sauvegarde de l'utilisateur et ajout à la liste des connectés
+      if (user !== undefined && userIndex === -1) { // S'il est bien nouveau, Sauvegarde de l'utilisateur et ajout à la liste des connectés
         loggedUser = user;
         users.push(loggedUser);
         // Envoi des messages de service
@@ -45,7 +60,7 @@ io.on('connection', function(socket) {
         };
         socket.emit('service-message', userServiceMessage);
         socket.broadcast.emit('service-message', broadcastedServiceMessage);
-        // Emission de 'user-login' et appel du callback
+        messages.push(broadcastedServiceMessage);
         io.emit('user-login', loggedUser);
         callback(true);
       }
@@ -54,23 +69,48 @@ io.on('connection', function(socket) {
       }
     });
 
-    socket.on('disconnect', function() {
-      if (loggedUser !== undefined) {
-        // Broadcast d'un 'service-message'
-        var serviceMessage = {
-          text: 'User "' + loggedUser.username + '" disconnected',
-          type: 'logout'
-        };
-        socket.broadcast.emit('service-message', serviceMessage);
-        // Suppression de la liste des connectés
-        var userIndex = users.indexOf(loggedUser);
-        if (userIndex !== -1) {
-          users.splice(userIndex, 1);
-        }
-      // Emission d'un 'user-logout' contenant le user
-        io.emit('user-logout', loggedUser);
+// L'utilisateur commence à taper
+  socket.on('start-typing', function () {
+   // Ajout du user à la liste des utilisateurs en cours de saisie
+   if (typingUsers.indexOf(loggedUser) === -1) {
+     typingUsers.push(loggedUser);
+   }
+  io.emit('update-typing', typingUsers);
+  });
+
+// L'utilisateur arrête de taper
+  socket.on('stop-typing', function () {
+    var typingUserIndex = typingUsers.indexOf(loggedUser);
+    if (typingUserIndex !== -1) {
+      typingUsers.splice(typingUserIndex, 1);
+    }
+    io.emit('update-typing', typingUsers);
+  });
+
+
+  socket.on('disconnect', function() {
+    if (loggedUser !== undefined) {
+      // Broadcast d'un 'service-message'
+      var serviceMessage = {
+        text: 'User "' + loggedUser.username + '" disconnected',
+        type: 'logout'
+      };
+      socket.broadcast.emit('service-message', serviceMessage);
+      // Suppression de la liste des connectés
+      var userIndex = users.indexOf(loggedUser);
+      if (userIndex !== -1) {
+        users.splice(userIndex, 1);
       }
-    });
+      messages.push(serviceMessage);
+      // Emission d'un 'user-logout' contenant le user
+      io.emit('user-logout', loggedUser);
+    }
+    // Si l'utilisateur se deconnecte pendant qu'il écrit, le supprimer de la liste d'utilisateurs
+    var typingUserIndex = typingUsers.indexOf(loggedUser);
+    if (typingUserIndex !== -1) {
+      typingUsers.splice(typingUserIndex, 1);
+    }
+  });
 });
 
 http.listen(8080, function() {
